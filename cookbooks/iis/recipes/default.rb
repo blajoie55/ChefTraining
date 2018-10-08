b979d1fa675f441e152d169768fe97adb1d070fe
Brad <blajoie55@gmail.com>
2018-10-08 14:27:49 -0400

    adding IIS cookbook and server recipe

---
+++ cookbooks/iis/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: iis
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
---
+++ cookbooks/iis/recipes/server.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: iis
+# Recipe:: server
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
